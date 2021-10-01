21f5f5c19af2744530e695b2bac214dddcb30daf
Oscar G. Rodrigues <o.rodrigues@groupeonepoint.com>
2021-10-01 15:30:30 +0200

    [SERVICES] Create "httpClient" to use same instance for API calls

---
+++ src/services/httpClient.ts
@@ -0,0 +1,45 @@
+import Axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+// Maybe we can add this endpoint as process env variable
+export const API_ROOT_URL: string = "http://localhost:3005/"
+
+class HttpClient {
+  static instance: HttpClient = new HttpClient()
+
+  axios: AxiosInstance
+
+  constructor() {
+    this.initInstance()
+  }
+
+  static getInstance() {
+    return HttpClient.instance
+  }
+
+  initInstance() {
+    this.axios = Axios.create({
+      baseURL: `${API_ROOT_URL}`,
+      headers: {
+        'Content-Type': "application/json",
+      }
+    })
+  }
+
+  get(url: string, config?: any): Promise<AxiosResponse> {
+    return this.axios.get(url, config)
+  }
+
+  post(url: string, data: any, config: any): Promise<AxiosResponse> {
+    return this.axios.post(url, data, config)
+  }
+
+  put(url: string, data: any, config?: any): Promise<AxiosResponse> {
+    return this.axios.put(url, data, config)
+  }
+
+  delete(url: string, config: any): Promise<AxiosResponse> {
+    return this.axios.delete(url,config)
+  }
+}
+
+export default HttpClient.getInstance()
