bbc76970502ebbec68bc77763b9d0319b580a1d7
Oscar G. Rodrigues <o.rodrigues@groupeonepoint.com>
2021-10-01 15:35:01 +0200

    [REFACTO] Change "MyApp" as arrow function component & add type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { FC, ReactElement } from 'react'
 import type { AppProps } from 'next/app'
-import { getLoggedUserId } from '../utils/getLoggedUserId'
-import '../styles/globals.css'
+import { getLoggedUserId } from 'utils/getLoggedUserId'
+import 'styles/globals.css'
 
 // Default way to get a logged user
-export const loggedUserId = getLoggedUserId()
+export const loggedUserId: number = getLoggedUserId()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: FC<AppProps> = ({ Component, pageProps }): ReactElement => (
+  <Component {...pageProps} />
+);
 
 export default MyApp
