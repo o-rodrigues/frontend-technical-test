24c9fe1faf4df4ce3f524d907821c3d2220024b2
Oscar G. Rodrigues <o.rodrigues@groupeonepoint.com>
2021-10-01 15:38:16 +0200

    [REFACTO] Rework "index.tsx" & "Home.module.css"   * Fetch "users/:id" to get logged user data   * Add Avatar placeholder   * Add Welcome logged user message   * Add link to future redirection to "/conversations/:userId"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
-import type { FC } from 'react'
+import type { FC, ReactElement } from 'react'
+import { AxiosResponse } from 'axios'
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Logo from '../assets/lbc-logo.webp'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-const Home: FC = () => {
-  const year = new Date().getFullYear()
+import Logo from 'assets/lbc-logo.webp'
+import Avatar from 'assets/missing_avatar.png'
+import { loggedUserId } from 'pages/_app'
+import httpClient from 'services/httpClient'
+import styles from 'styles/Home.module.css'
+import { User } from 'types/user'
+
+interface IProps {
+  user: User
+}
+
+const Home: FC<IProps> = ({ user }): ReactElement => {
+  const year: number = new Date().getFullYear()
 
   return (
     <div className={styles.container}>
@@ -14,47 +26,16 @@ const Home: FC = () => {
         <meta name="description" content="Frontend exercise for developpers who want to join us on leboncoin.fr"></meta>
       </Head>
 
-      <main className={styles.main}>
+      <header className={styles.header}>
         <Image src={Logo} alt="Leboncoin's logo" width={400} height={125} />
-        <h1 className={styles.title}>
-          Welcome !
-        </h1>
+      </header>
 
-        <p className={styles.description}>
-          This test is based on a <a href="https://nextjs.org/docs/getting-started" target="_blank" rel="noopener noreferrer">Next.js</a> application.<br />
-          Fork the repository and use the <code className={styles.code}>main</code> branch as your starting point.
-          <br /><br />
-
-          Get started by reading{' '}
-          <code className={styles.code}>README.md</code> and editing <code className={styles.code}>src/pages/index.js</code>
-          <br />
-          Once you are done, send the repository link to your HR contact.
-        </p>
-
-        <div className={styles.grid}>
-          <article className={styles.card}>
-            <h2>Design</h2>
-            <p>Feel free to create any design you want for this exercise. Let your creativity talks !</p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>Libraries</h2>
-            <p>Feel free to use any library you want. Only Next.js / React are required.</p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>API Server</h2>
-            <p>
-              Start the API server on port <code className={styles.code}>3005</code> by running<br /><code className={styles.code}>npm run start-server</code>.<br/>
-              Find the swagger definitions in <code className={styles.code}>docs/api-swagger.yml</code> or <a href="https://leboncoin.tech/frontend-technical-test/" target="_blank" rel="noopener noreferrer">here</a>.
-            </p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>Timing</h2>
-            <p>We recommend 4 hours for this test. You are free to spend more (or less) time, let us know how much time did you spend.</p>
-          </article>
-        </div>
+      <main className={styles.main}>
+        <Image src={Avatar} alt="user's avatar" width={125} height={125} />
+        <h1 className={styles.title}>Welcome, {user.nickname} !</h1>
+        <Link href={`/`}>
+          <a className={styles.link}>Check your conversations</a>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
@@ -62,6 +43,13 @@ const Home: FC = () => {
       </footer>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async() => {
+  const res: AxiosResponse<User>  = await httpClient.get(`users/${loggedUserId}`)
+  const user: User = await res.data
+
+  return { props: { user } }
 }
 
 export default Home
